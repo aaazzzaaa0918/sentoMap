a35c6ab90dd6ad803921bad2cdd56d6e2f9ac42e
azama_motokazu <azama_motokazu@cyberagent.co.jp>
2013-12-04 11:56:51 +0900

    create css & list

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,9 +1,13 @@
+# encoding: utf-8
+
 class MapsController < ApplicationController
   def index
+  	@pageTitle = '銭湯マップ | リスト'
   	@mapData = Map.all
   end
 
   def show
+  	@pageTitle = '銭湯マップ | 詳細'
   	@mapDetail = Map.find(params[:id])
   end
 end
